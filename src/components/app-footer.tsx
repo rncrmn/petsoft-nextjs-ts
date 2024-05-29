7760381dcb4d78afc4df9db07431e3c3793197ca
leadgeex <aaronleadgeex@gmail.com>
2024-05-29 22:53:46 +0800

    footer update

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function AppFooter() {
-    return (
-        <footer className="mt-auto border-t border-black/5 py-5">
-            <small className="opacity-50">
-                &copy; 2030 ByteGrad. All rights reserved.
-            </small>
-        </footer>
-    );
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5">
+      <small className="opacity-50">
+        &copy; 2024 PetSoft. All rights reserved. Created by{" "}
+        <Link href="https://github.com/rncrmn" target="_blank">
+          Aaron Carmen
+        </Link>
+        .
+      </small>
+    </footer>
+  );
 }
